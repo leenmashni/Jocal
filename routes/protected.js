5c1de6ad6e3e2937f0bb68b2249106f40319e091
sawsan <sos0217849@ju.edu.jo>
2025-05-10 14:45:20 +0300

    First commit - added initial backend files

---
+++ routes/protected.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+
+router.get('/test', authMiddleware, (req, res) => {
+  res.json({ message: 'Protected route accessed successfully!', user: req.user });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,27 @@
+//server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    //_______________________________________________________
+app.use('/api/protected', require('./routes/protected'));
+//___________________________________________________________
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error('DB connection error:', err));
+  
+  
